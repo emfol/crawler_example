d6fc4a2e50b03fb3856fbc82bb781d29b3b5f778
Emanuel F. Oliveira <emfolg@gmail.com>
2020-05-03 15:35:31 -0300

    [WIP] Finalizing main crawler

--- index.js
+++ index.js
@@ -2,23 +2,25 @@
  * Imports
  */
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const api = require("./lib/api");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const api = require('./lib/api');
+const {log4js} = require('./lib/log4js');
 
 /**
  * Constants
  */
 
 const PORT = 8080;
-const PUBLIC_DIR = "public";
+const PUBLIC_DIR = 'public';
 
 /**
  * Initialization
  */
 
 const app = express();
+const logger = log4js.getLogger('App');
 
 /**
  * Middlewares
@@ -31,20 +33,17 @@ app.use(express.static(path.join(__dirname, PUBLIC_DIR)));
  * Endpoints
  */
 
-app.get("/ping", function (req, res) {
-  return res.json({ message: "pong" });
-});
+app.get('/ping', (req, res) => res.json({message: 'pong'}));
 
-app.get("/search", api.search);
+app.get('/search', api.search);
 
 const port = process.env.PORT || PORT;
 app
-  .listen(port, () => {
-    console.info("Server running at:", port);
-  })
-  .on("error", error => {
-    if (error) {
-      console.error("Failed to start server...", error.code);
-      return;
-    }
-  });
+    .listen(port, () => {
+      logger.info('Server running at:', port);
+    })
+    .on('error', (error) => {
+      if (error) {
+        logger.error('Failed to start server...', error.code);
+      }
+    });
--- public/main.js
+++ public/main.js
@@ -1,3 +1,3 @@
-(function () {
-  console.log("Script loaded...");
+(function() {
+  console.log('Script loaded...');
 }());
